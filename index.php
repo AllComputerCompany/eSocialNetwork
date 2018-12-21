dc3827384b4b54bd31768b2b906db0994e86536e
Bachir Zerourou <Bachir bachir.zerourou@yahoo.fr>
2018-12-21 12:38:37 +0100

    First release of eSocialMedia

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> eSocialMedia</title>
+
+
+</head>
+<body>
+
+
+    <h1>  eSocialMedia</h1>
+    
+	<p> this is a small platform for a social media </p>
+
+</body>
+</html>
