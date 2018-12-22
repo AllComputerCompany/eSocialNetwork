8e6c0de5868e29b448f07d8d0992b21576d7949e
Bachir Zerourou <Bachir bachir.zerourou@yahoo.fr>
2018-12-22 15:51:15 +0100

    fix bootswatch cdn 3.3.7 readable

--- partials/_header.php
+++ partials/_header.php
@@ -18,11 +18,13 @@
 
   <link rel="stylesheet" type="text/css" href="asset/css/main.css">
 
-  <!-- bootstrap 2.3.2-->
-  <link href="https://stackpath.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" integrity="sha384-4FeI0trTH/PCsLWrGCD1mScoFu9Jf2NdknFdFoJhXZFwsvzZ3Bo5sAh7+zL8Xgnd" crossorigin="anonymous">
+  
 
   <!--bootstrap watch 3.3.7 reanable-->
-  <!--<link href="https://stackpath.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-Li5uVfY2bSkD3WQyiHX8tJd0aMF91rMrQP5aAewFkHkVSTT2TmD2PehZeMmm7aiL" crossorigin="anonymous">-->
+  <link href="https://stackpath.bootstrapcdn.com/bootswatch/3.3.7/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-Li5uVfY2bSkD3WQyiHX8tJd0aMF91rMrQP5aAewFkHkVSTT2TmD2PehZeMmm7aiL" crossorigin="anonymous">
+
+
+ 
 
 </head>
 
--- partials/_nav.php
+++ partials/_nav.php
@@ -1,22 +1,28 @@
 
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-      <div class="container">
-        <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+    <div class="container">
+
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#exp-nav-collapse">
+          <span class="rc-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="brand" href="#">Social Network</a>
-        <div class="nav-collapse collapse">
-          <ul class="nav">
+        <a class="navbar-brand" href="#">Social Network</a>
+       </div>
+
+        <div class="collapse navbar-collapse" id="exp-nav-collapse">
+          <ul class="nav navbar-nav">
             <li class="active"><a href="index.php">Accueil</a></li>
             <li > <a href="#"> Connexion</a></li>
             <li><a href="register.php">Inscription</a></li>
             <li><a href="#contact">Contacter</a></li>
           </ul>
         </div><!--/.nav-collapse -->
-      </div>
+     
     </div>
-  </div>
+
+  </nav>
